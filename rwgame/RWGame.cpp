2cf4000d4b9209a450eaa8df8faa75a2059e9127
Daniel Evans <danharibo@gmail.com>
2016-06-01 01:15:24 +0100

    Add suspension debug rendering to rwgame

--- rwgame/RWGame.cpp
+++ rwgame/RWGame.cpp
@@ -34,8 +34,9 @@ RWGame::RWGame(int argc, char* argv[])
 	: config("openrw.ini")
 	, state(nullptr), world(nullptr), renderer(nullptr), script(nullptr),
 	debugScript(false), inFocus(true),
-	showDebugStats(false), showDebugPaths(false), showDebugPhysics(false),
-	accum(0.f), timescale(1.f)
+	showDebugStats(false), showDebugPaths(false), showDebugPhysics(false)
+  , showDebugSuspension(false)
+  , accum(0.f), timescale(1.f)
 {
 	if (!config.isValid())
 	{
@@ -571,6 +572,12 @@ void RWGame::render(float alpha, float time)
 			debug->flush(renderer);
 		}
 	}
+
+	if (showDebugSuspension)
+	{
+		renderSuspension();
+		debug->flush(renderer);
+	}
 	RW_PROFILE_END();
 	
 	drawOnScreenText(world, renderer);
@@ -779,6 +786,28 @@ void RWGame::renderProfile()
 #endif
 }
 
+void RWGame::renderSuspension()
+{
+	btVector3 minColor(1.f, 0.f, 0.f);
+	for (const auto& entry : world->vehiclePool.objects)
+	{
+		VehicleObject* vehicle = static_cast<VehicleObject*>(entry.second);
+
+		for (const VehicleDynamics::WheelInfo& wheel : vehicle->dynamics.getWheels())
+		{
+			auto b = vehicle->getPosition() + vehicle->getRotation() * wheel.position;
+			auto f1 = wheel.suspensionForceWS / 1000.f;
+			auto f2 = wheel.tractionForceWS / 1000.f;
+			btVector3 suspensionBegin(b.x, b.y, b.z);
+			btVector3 suspensionEnd(b.x + f1.x, b.y + f1.y, b.z + f1.z);
+			debug->drawLine(suspensionBegin, suspensionEnd, minColor);
+			btVector3 tractionBegin(b.x, b.y, b.z);
+			btVector3 tractionEnd(b.x + f2.x, b.y + f2.y, b.z + f2.z);
+			debug->drawLine(tractionBegin, tractionEnd, minColor);
+		}
+	}
+}
+
 void RWGame::globalKeyEvent(const sf::Event& event)
 {
 	switch (event.key.code) {
@@ -802,6 +831,9 @@ void RWGame::globalKeyEvent(const sf::Event& event)
 		break;
 	case sf::Keyboard::F3:
 		showDebugPhysics = ! showDebugPhysics;
+		break;
+	case sf::Keyboard::F4:
+		showDebugSuspension = ! showDebugSuspension;
 		break;
 	case sf::Keyboard::F12: {
 		auto homedir = getenv("HOME");
--- rwgame/RWGame.hpp
+++ rwgame/RWGame.hpp
@@ -37,6 +37,7 @@ class RWGame
 	bool showDebugStats;
 	bool showDebugPaths;
 	bool showDebugPhysics;
+	bool showDebugSuspension;
 	int lastDraws; /// Number of draws issued for the last frame.
 
 	float accum;
@@ -135,6 +136,7 @@ private:
 	void renderDebugStats(float time, Renderer::ProfileInfo& worldRenderTime);
 	void renderDebugPaths(float time);
 	void renderProfile();
+	void renderSuspension();
 
 	void globalKeyEvent(const sf::Event& event);
 };
